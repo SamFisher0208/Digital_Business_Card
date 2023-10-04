9f6f0d64dc885cfb447cb260ba7fe13c559982c7
Sam Fisher <sammee1@gmail.com>
2023-10-03 22:48:32 -0500

    added footer styles

--- digital_business_card/src/components/Footer.tsx
+++ digital_business_card/src/components/Footer.tsx
@@ -1,9 +1,15 @@
 import { FaGithubSquare } from "react-icons/fa";
 
 export default function Footer() {
+  const link = "https://github.com/SamFisher0208";
+
+  const handleIconClick = () => {
+    link ? window.open(link, "_blank") : null;
+  };
+
   return (
     <footer className="footer">
-      <FaGithubSquare></FaGithubSquare>
+      <FaGithubSquare size={42} onClick={handleIconClick} className="github" />
     </footer>
   );
 }
