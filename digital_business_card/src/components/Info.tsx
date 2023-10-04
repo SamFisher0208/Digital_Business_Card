e046c382bf773f7b3e5986867e80f03e08103dd6
Sam Fisher <sammee1@gmail.com>
2023-10-03 23:53:18 -0500

    added simple media query

--- digital_business_card/src/components/Info.tsx
+++ digital_business_card/src/components/Info.tsx
@@ -3,25 +3,14 @@ export default function Info() {
     <>
       <h2>About</h2>
       <p className="about">
-        Hey there, I'm Sam and I'm thrilled to jump into the dynamic realm of
-        web developement.
-        <br />
-        <br />
-        After spending four years in administrative roles in Higher Ed, I felt a
-        calling to pursue a career that would rekindle that childhood excitement
-        of building and creating.
+        Hey there, I'm Sam, and I'm passionate about crafting user-friendly and
+        visually appealing digital experiences.
       </p>
       <h2>Interests</h2>
       <p className="interests">
         Apart from my professional life, I have a great passion for art. I’ve
         been studying traditionally and digitally for the past 10 years,
         designing characters, logos, and environments for fun and for clients.
-        <br />
-        <br />
-        Frontend development offers me the perfect opportunity to continually
-        learn, grow, and harness my passion for crafting user-friendly and
-        visually appealing digital experiences. It's a world where creativity
-        meets technology, and I can't wait to dive in!
       </p>
     </>
   );
